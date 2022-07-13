0b64992c448d73a127ae2c89d64cbf4ec9804a95
Belousov <belousov.work42@gmail.com>
2022-07-13 14:32:41 +0300

    move Ree::PackageDSL

---
+++ ree/lib/ree/package_dsl.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal  = true
+
+module Ree::PackageDSL
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  def self.extended(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def package(&proc)
+      path = caller[0].split(':').first
+
+      dsl = Ree::PackageDsl.new(
+        Ree.container.packages_facade, self, path
+      )
+
+      dsl.instance_exec(&proc) if block_given?
+
+      return if dsl.package.preloaded?
+      dsl.package.set_preloaded(true)
+
+      dsl.package.preload.each do |env, list|
+        next if !Ree.preload_for?(env)
+
+        list.each do |object_name|
+          Ree.container.compile_object(
+            "#{dsl.package.name}/#{object_name}",
+          )
+        end
+      end
+    end
+  end
+end
