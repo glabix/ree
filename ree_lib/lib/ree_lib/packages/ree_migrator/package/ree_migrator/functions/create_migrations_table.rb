8e62fe03fafcbe8b4322b88bbb85cc7e41b395c4
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2022-07-14 16:19:02 +0300

    remove type from primary_key for migrations table

--- ree_lib/lib/ree_lib/packages/ree_migrator/package/ree_migrator/functions/create_migrations_table.rb
+++ ree_lib/lib/ree_lib/packages/ree_migrator/package/ree_migrator/functions/create_migrations_table.rb
@@ -16,7 +16,7 @@ class ReeMigrator::CreateMigrationsTable
   def call(connection)
     if !connection.tables.include?(:migrations)
       connection.create_table :migrations do
-        primary_key :id, type: :integer
+        primary_key :id
         column :filename, "varchar(1024)", null: false
         column :created_at, DateTime, null: false
         column :type, "varchar(16)", null: false
