4d41fd0ec7626331aa370205434161f5dc5995f8
Konstantin Kim <kimkonsts@gmail.com>
2022-07-14 16:41:16 +0300

    factory logger spec

--- ree_lib/lib/ree_lib/packages/ree_logger/spec/ree_logger/beans/logger_spec.rb
+++ ree_lib/lib/ree_lib/packages/ree_logger/spec/ree_logger/beans/logger_spec.rb
@@ -4,8 +4,59 @@ package_require('ree_logger/beans/logger')
 
 RSpec.describe :logger do
   link :logger, from: :ree_logger
+  link :is_blank, from: :ree_object
+
+  before(:all) do
+    tmp_file_log = ENV['LOG_FILE_PATH']
+    
+    if !is_blank(tmp_file_log)
+      File.open(tmp_file_log, 'w') {|file| file.truncate(0) }
+    end
+  end
+
+  let(:log_file_path) { ENV['LOG_FILE_PATH'] }
+
+  let(:exception) {
+    StandardError.new('Some Exception')
+  }
 
   it {
-    logger.info('hello world')
+    expect { logger.info('hello world') }.to output(/hello world/).to_stdout
+    expect(File.read(log_file_path)).to match("hello world")
+  }
+
+  it {
+    expect { logger.info('hello world', { param: 1, another_param: { name: 'John'} }) }.to output(/John/).to_stdout
+    expect(File.read(log_file_path)).to match("John")
+  }
+  
+  it {
+    expect { logger.debug('debug message') }.to_not output(/debug message/).to_stdout
+    expect(File.read(log_file_path)).to_not match("debug")
+  }
+
+  it {
+    expect { logger.warn('warning message') }.to output(/warning message/).to_stdout
+    expect(File.read(log_file_path)).to match("warning message")
+  }
+
+  it {
+    output = with_captured_stdout {
+      logger.error('some error message', {}, exception, false)
+    }
+    expect(output).to match(/some error message/)
+    expect(output).to_not match(/method|args/)
+    expect(File.read(log_file_path)).to match("some error message")
+    expect(File.read(log_file_path)).to_not match("PARAMETERS: {:method=>{:name=>:call, :args=>{:block=>{}}}}")
+  }
+
+  it {
+    expect { logger.fatal('some fatal message', {}, exception) }.to output(/some fatal message/).to_stdout
+    expect(File.read(log_file_path)).to match("some fatal message")
+  }
+
+  it {
+    expect { logger.unknown('unknown message') }.to output(/unknown message/).to_stdout
+    expect(File.read(log_file_path)).to match("unknown message")
   }
 end
--- ree_lib/lib/ree_lib/packages/ree_logger/spec/spec_helper.rb
+++ ree_lib/lib/ree_lib/packages/ree_logger/spec/spec_helper.rb
@@ -2,7 +2,17 @@ RSpec.configure do |config|
   config.extend Ree::RSpecLinkDSL
 end
 
+def with_captured_stdout
+  original_stdout = $stdout
+  $stdout = StringIO.new
+  yield
+  $stdout.string
+ensure
+  $stdout = original_stdout
+end
+
 ENV['LOG_FILE_PATH'] = '/tmp/ree_logger.log'
+ENV['LOG_FILE_AUTO_FLUSH'] = 'true'
 ENV['LOG_LEVEL_FILE'] = 'info'
 ENV['LOG_LEVEL_STDOUT'] = 'info'
 ENV['LOG_RATE_LIMIT_INTERVAL'] = '60'
