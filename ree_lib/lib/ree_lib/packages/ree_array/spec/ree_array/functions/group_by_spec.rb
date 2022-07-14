7b0ba0c8ef7a392a7fa79c9ad4166d8a348682a8
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2022-07-14 14:10:20 +0300

    ree_array#group_by & ree_array#index_by

---
+++ ree_lib/lib/ree_lib/packages/ree_array/spec/ree_array/functions/group_by_spec.rb
@@ -0,0 +1,18 @@
+#frozen_string_literal = true
+
+package_require('ree_array/functions/group_by')
+
+RSpec.describe :group_by do
+  link :group_by, from: :ree_array
+
+  it {
+    list = [ {id: 1, name: 'John'}, {id: 1, name: 'Smith'} ]
+    result = group_by(list) { _1[:id] }
+
+    expect(result).to eq(
+      {
+        1 => [ {id: 1, name: 'John'}, {id: 1, name: 'Smith'} ]
+      }
+    )
+  }
+end
---
+++ ree_lib/lib/ree_lib/packages/ree_array/spec/ree_array/functions/index_by_spec.rb
@@ -0,0 +1,17 @@
+#frozen_string_literal = true
+
+RSpec.describe :index_by do
+  link :index_by, from: :ree_array
+
+  it {
+    list = [ {id: 1}, {id: 2} ]
+    result = index_by(list) { _1[:id] }
+
+    expect(result).to eq(
+      {
+        1 => {id: 1},
+        2 => {id: 2}
+      }
+    )
+  }
+end
