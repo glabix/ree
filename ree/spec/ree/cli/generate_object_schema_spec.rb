86b158b69812a53fe9578f2b6d0390ce9750b742
Konstantin Kim <kimkonsts@gmail.com>
2022-10-29 15:24:40 +0300

    remove unnecessary spec

--- ree/spec/ree/cli/generate_object_schema_spec.rb
+++ ree/spec/ree/cli/generate_object_schema_spec.rb
@@ -21,52 +21,6 @@ RSpec.describe Ree::CLI::GenerateObjectSchema do
       end
     end
 
-    it "generates schema.json for new object" do
-      before_schema = File.open(File.join(project_dir, "bc/#{package_name}/Package.schema.json")).read
-      new_object_file_rpath = "bc/accounts/package/accounts/commands/definitely_new_object.rb"
-      new_object_file_abs_path = File.join(project_dir, new_object_file_rpath)
-
-      new_object_content = <<-NEW_OBJECT
-      class Accounts::DefinitelyNewObject
-        include Ree::FnDSL
-      
-        fn :definitely_new_object do
-        end
-      
-        contract(None => nil)
-        def call()
-        end
-      end
-      NEW_OBJECT
-
-      File.open(
-        new_object_file_abs_path,
-        'w'
-      ) { |f| f.write new_object_content }
-
-      subject.run(
-        package_name: package_name,
-        object_path: new_object_file_rpath,
-        project_path: project_dir,
-        silence: true
-      )
-
-      FileUtils.cd(object_schema_dir) do
-        ensure_exists("definitely_new_object.schema.json")
-      end
-
-      FileUtils.cd(project_dir) do
-        package_schema = JSON.parse(File.open(File.join(project_dir, "bc", "accounts", "Package.schema.json")).read)
-        expect(package_schema.dig("objects").find { |o| o["name"] == "definitely_new_object" }).to_not eq(nil)
-      end
-
-      FileUtils.rm(new_object_file_abs_path)
-      FileUtils.rm(File.join(object_schema_dir, "definitely_new_object.schema.json"))
-      # TODO: package schema stays the same if we're working in same container
-      # do we need to implement some reload mechanism for objects_store?
-      File.open(File.join(project_dir, "bc/#{package_name}/Package.schema.json"), 'w') { |f| f.write before_schema }
-    end
-
     it "show error for wrong object path" do
       expect {
         subject.run(
