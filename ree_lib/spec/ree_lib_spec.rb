9ee89b418389e145d3a5c3aef96d97572ec42887
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2022-12-20 15:33:14 +0300

    bundle install

--- ree_lib/spec/ree_lib_spec.rb
+++ ree_lib/spec/ree_lib_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe ReeLib do
   it "has a version number" do
     expect(ReeLib::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
