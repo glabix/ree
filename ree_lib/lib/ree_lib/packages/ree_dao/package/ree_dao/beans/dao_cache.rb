144d31019f1fdc23c3783c6c71da7919100eec54
Konstantin Kim <kimkonsts@gmail.com>
2023-04-13 15:52:16 +0300

    fix: check for thread cache

--- ree_lib/lib/ree_lib/packages/ree_dao/package/ree_dao/beans/dao_cache.rb
+++ ree_lib/lib/ree_lib/packages/ree_dao/package/ree_dao/beans/dao_cache.rb
@@ -35,11 +35,6 @@ class ReeDao::DaoCache
     @thread_groups[current_thread_group.object_id][table_name][primary_key] = deep_dup(data)
   end
 
-  def drop_table_cache(table_name)
-    add_thread_group_cache(current_thread_group)
-    @thread_groups[current_thread_group.object_id].delete(table_name)
-  end
-
   private
 
   def current_thread_group
@@ -47,10 +42,22 @@ class ReeDao::DaoCache
   end
 
   def add_table_name(thread_group = Thread.current.group, table_name)
+    if !@thread_groups[thread_group.object_id]
+      @thread_groups[thread_group.object_id] ||= {}
+    end
+
     @thread_groups[thread_group.object_id][table_name] ||= {}
   end
 
   def add_primary_key(thread_group = Thread.current.group, table_name, primary_key)
+    if !@thread_groups[thread_group.object_id]
+      @thread_groups[thread_group.object_id] ||= {}
+    end
+
+    if !thread_groups[thread_group.object_id][table_name]
+      @thread_groups[thread_group.object_id][table_name] ||= {}
+    end
+
     @thread_groups[thread_group.object_id][table_name][primary_key] ||= {}
   end
 end
