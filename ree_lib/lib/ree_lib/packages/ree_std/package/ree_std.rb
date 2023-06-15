c8cf1b06fb6649c256dae62a9a7629efdb321e58
Nikita Kononov <vocrsz@gmail.com>
2023-06-15 14:22:04 +0200

    Update ree_std.rb

--- ree_lib/lib/ree_lib/packages/ree_std/package/ree_std.rb
+++ ree_lib/lib/ree_lib/packages/ree_std/package/ree_std.rb
@@ -2,14 +2,5 @@ module ReeStd
   include Ree::PackageDSL
 
   package do
-    # Example of usage:
-    # tags ['wip']
-    # depends_on :package_name
-    # env_var 'ENV_VAR_NAME'
-    # preload(
-    #   production: [
-    #     :bean_or_fn
-    #   ]  
-    # )
   end
 end
