18a20241d482bac370fa18ada6e14d2217a716ec
Ruslan Gatiyatov <ruslan.gatiyatov@gmail.com>
2024-07-30 12:25:16 +0300

    remove object schema generator

--- ree/lib/ree/cli.rb
+++ ree/lib/ree/cli.rb
@@ -5,8 +5,6 @@ module Ree
     autoload :Init, 'ree/cli/init'
     autoload :GeneratePackagesSchema, 'ree/cli/generate_packages_schema'
     autoload :GeneratePackageSchema, 'ree/cli/generate_package_schema'
-    autoload :GenerateObjectSchema, 'ree/cli/generate_object_schema'
-    autoload :DeleteObjectSchema, 'ree/cli/delete_object_schema'
     autoload :GeneratePackage, 'ree/cli/generate_package'
     autoload :GenerateTemplate, 'ree/cli/generate_template'
     autoload :Indexing, 'ree/cli/indexing'
