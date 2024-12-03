f4c49109e9b8894aa65d0ebf29204377f94ea7eb
Oleg Potapov <oleg0potapov+1@gmail.com>
2024-12-03 15:14:58 +0300

    fix import dsl const missing

--- ree/lib/ree/dsl/import_dsl.rb
+++ ree/lib/ree/dsl/import_dsl.rb
@@ -30,7 +30,7 @@ class Ree::ImportDsl
     retry
   end
 
-  def const_missing(const_name)
+  def self.const_missing(const_name)
     puts "import dsl const missing"
     raise NameError.new("class not found #{const_name.to_s}", const_name)
   end
