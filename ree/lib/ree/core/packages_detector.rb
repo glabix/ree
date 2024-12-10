454c35f9adead3097b330fc137786581bea5749e
Oleg Potapov <oleg0potapov+1@gmail.com>
2024-12-10 18:48:13 +0300

    modify package detector to detect packages without schemas

--- ree/lib/ree/core/packages_detector.rb
+++ ree/lib/ree/core/packages_detector.rb
@@ -10,9 +10,11 @@ class Ree::PackagesDetector
       raise Ree::Error.new("dir does not exist: #{dir}", :invalid_dir)
     end
 
-    files = File.join(dir, "**/", Ree::PACKAGE_SCHEMA_FILE)
     names = {}
+    packages = []
 
+    # find by schema file
+    files = File.join(dir, "**/", Ree::PACKAGE_SCHEMA_FILE)
     Dir[files].map do |file|
       package_schema_path = Pathname
         .new(file)
@@ -32,12 +34,38 @@ class Ree::PackagesDetector
 
       names[name] = entry_path
 
-      {
+      packages << {
         name: name.to_sym,
         entry_path: entry_path,
         package_schema_path: package_schema_path,
         gem_name: gem_name
       }
     end
+
+    package_dirs = File.join(dir, "**/package")
+    Dir[package_dirs].each do |package_dir|
+      next unless File.directory?(package_dir)
+
+      dir_path = Pathname.new(package_dir)
+      name = dir_path.parent.basename.to_s
+
+      next if names.has_key?(name)
+
+      package_rel_path = dir_path.relative_path_from(dir)
+      parent_rel_path = dir_path.parent.relative_path_from(dir)
+
+      entry_path = Ree::PathHelper.package_entry_path(package_rel_path)
+
+      names[name] = entry_path
+
+      packages << {
+        name: name.to_sym,
+        entry_path: entry_path,
+        package_schema_path: File.join(parent_rel_path, Ree::PACKAGE_SCHEMA_FILE),
+        gem_name: gem_name
+      }
+    end
+
+    packages
   end
 end
