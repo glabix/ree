38d48bffb4aab55478211ec55f8af12509a419c6
Oleg Potapov <oleg0potapov+1@gmail.com>
2024-12-22 23:39:34 +0300

    remove indexing by schema from package detector

--- ree/lib/ree/core/packages_detector.rb
+++ ree/lib/ree/core/packages_detector.rb
@@ -13,35 +13,6 @@ class Ree::PackagesDetector
     names = {}
     packages = []
 
-    # find by schema file
-    files = File.join(dir, "**/", Ree::PACKAGE_SCHEMA_FILE)
-    Dir[files].map do |file|
-      package_schema_path = Pathname
-        .new(file)
-        .relative_path_from(Pathname.new(dir))
-        .to_s
-
-      name = package_schema_path.split('/')[-2]
-      entry_path = Ree::PathHelper.package_entry_path(package_schema_path)
-
-      if !File.exist?(File.join(dir, entry_path))
-        Ree.logger.error("Entry file does not exist for '#{name}' package: #{entry_path}")
-      end
-
-      if names.has_key?(name)
-        raise Ree::Error.new("package '#{name}' has duplicate defintions.\n\t1) #{names[name]},\n\t2) #{entry_path}", :duplicate_package)
-      end
-
-      names[name] = entry_path
-
-      packages << {
-        name: name.to_sym,
-        entry_path: entry_path,
-        package_schema_path: package_schema_path,
-        gem_name: gem_name
-      }
-    end
-
     package_dirs = File.join(dir, "**/package")
     Dir[package_dirs].each do |package_dir|
       next unless File.directory?(package_dir)
