91aa6282425b80ff5744c3dbb8be250d69b88bbf
Oleg Potapov <oleg0potapov+1@gmail.com>
2025-01-20 15:29:48 +0300

    parse fn signatures

--- ree/lib/ruby_lsp/ree/ree_indexing_enhancement.rb
+++ ree/lib/ruby_lsp/ree/ree_indexing_enhancement.rb
@@ -4,13 +4,33 @@ class ReeIndexingEnhancement < RubyIndexer::Enhancement
 
     # Return early unless the method call is the one we want to handle
     return unless node.name == :fn
+    return unless node.arguments
      
     location = node.location
-    
+    fn_name = node.arguments.child_nodes.first.unescaped
+
+    signatures = parse_signatures(fn_name)
+
     @listener.add_method(
-      node.arguments.child_nodes.first.unescaped,
+      fn_name,
       location, 
-      []
+      signatures,
+      comments: "ree object"
     )
+  end
+
+  def parse_signatures(fn_name)
+    uri = @listener.instance_variable_get(:@uri)
+    ast = Prism.parse_file(uri.path).value
+
+    class_node = ast.statements.body.detect{ |node| node.is_a?(Prism::ClassNode) }
+    return [] unless class_node
+
+    call_node = class_node.body.body.detect{ |node| node.name == :call }
+    return [] unless call_node
+    
+    signature_params = @listener.send(:list_params, call_node.parameters)
+
+    [RubyIndexer::Entry::Signature.new(signature_params)]
   end
 end
