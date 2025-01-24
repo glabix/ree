2ac960f3935ae633c861c3f888b28f025399e34b
Oleg Potapov <oleg0potapov+1@gmail.com>
2025-01-24 13:19:31 +0300

    don't add link if already added

--- ree/lib/ruby_lsp/ree/completion.rb
+++ ree/lib/ruby_lsp/ree/completion.rb
@@ -4,6 +4,7 @@ module RubyLsp
       include Requests::Support::Common
 
       REE_PREFIX = '__ree_object_'
+      CHARS_COUNT = 4
       
       def initialize(response_builder, node_context, index, dispatcher, uri)
         @response_builder = response_builder
@@ -15,9 +16,13 @@ module RubyLsp
 
       def on_call_node_enter(node)
         return if node.receiver
-        return if node.name.to_s.size < 5
+        return if node.name.to_s.size < CHARS_COUNT
 
         ree_objects = @index.prefix_search(REE_PREFIX + node.name.to_s).take(10)
+
+        return if ree_objects.size == 0
+
+        doc_info = parse_doc_info()
 
         ree_objects.each do |ree_obj|
           ree_object = ree_obj.first
@@ -48,7 +53,7 @@ module RubyLsp
               owner_name: "Object",
               guessed_type: false,
             },
-            additional_text_edits: get_additional_text_edits(fn_name, package_name)
+            additional_text_edits: get_additional_text_edits(doc_info, fn_name, package_name)
             # documentation: create_documentation(fn_name, params_str, ree_object, uri_parts)
           )
         end
@@ -56,17 +61,15 @@ module RubyLsp
         nil
       end
 
-      def get_additional_text_edits(fn_name, package_name)
-        ast = Prism.parse_file(@uri.path).value
-
-        class_node = ast.statements.body.detect{ |node| node.is_a?(Prism::ClassNode) }
-        fn_node = class_node.body.body.detect{ |node| node.name == :fn }
-        block_node = fn_node.block
-
-        return [] unless block_node # TODO handle no block
-        # links = block_node.body.body.select{ |node| node.name == :link }
+      def get_additional_text_edits(doc_info, fn_name, package_name)
+        return [] unless doc_info.block_node # TODO handle no block
         
-        fn_line = fn_node.location.start_line
+        if doc_info.linked_objects.map(&:name).include?(fn_name)
+          $stderr.puts("links already include #{fn_name}")
+          return []
+        end
+
+        fn_line = doc_info.fn_node.location.start_line
         position = 80 # TODO calc
 
         link_text = "\n\s\s\s\slink :#{fn_name}, from: :#{package_name}"
@@ -98,6 +101,46 @@ module RubyLsp
         """
 
         Interface::MarkupContent.new(kind: 'markdown', value: documentation)
+      end
+
+      def parse_doc_info
+        ast = Prism.parse_file(@uri.path).value
+
+        class_node = ast.statements.body.detect{ |node| node.is_a?(Prism::ClassNode) }
+        fn_node = class_node.body.body.detect{ |node| node.name == :fn }
+        block_node = fn_node.block
+
+        link_nodes = if block_node
+          block_node.body.body.select{ |node| node.name == :link }
+        else
+          []
+        end
+
+        linked_objects = link_nodes.map do |link_node|
+          name_arg_node = link_node.arguments.arguments.first
+
+          name_val = case name_arg_node
+          when Prism::SymbolNode
+            name_arg_node.value
+          when Prism::StringNode
+            name_arg_node.unescaped
+          else
+            ""
+          end
+
+          OpenStruct.new(
+            name: name_val
+          )
+        end
+
+        return OpenStruct.new(
+          ast: ast,
+          class_node: class_node,
+          fn_node: fn_node,
+          block_node: block_node,
+          link_nodes: link_nodes,
+          linked_objects: linked_objects
+        )
       end
     end
   end
