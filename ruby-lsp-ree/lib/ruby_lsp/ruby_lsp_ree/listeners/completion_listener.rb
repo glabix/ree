8b3ec03cd59f3992981742421768d29b68bd4e53
Oleg Potapov <oleg0potapov+1@gmail.com>
2025-02-18 15:00:49 +0300

    extract completion handler

--- ruby-lsp-ree/lib/ruby_lsp/ruby_lsp_ree/listeners/completion_listener.rb
+++ ruby-lsp-ree/lib/ruby_lsp/ruby_lsp_ree/listeners/completion_listener.rb
@@ -1,22 +1,15 @@
-require_relative "../utils/ree_lsp_utils"
-require_relative "../utils/completion_utils"
-require_relative "../ree_object_finder"
+require_relative "../handlers/completion_handler"
 
 module RubyLsp
   module Ree
     class CompletionListener
       include Requests::Support::Common
-      include RubyLsp::Ree::ReeLspUtils
-      include RubyLsp::Ree::CompletionUtils
 
       CHARS_COUNT = 1
-      CANDIDATES_LIMIT = 100
       
       def initialize(response_builder, node_context, index, dispatcher, uri)
         @response_builder = response_builder
-        @index = index
-        @uri = uri
-        @node_context = node_context
+        @handler = RubyLsp::Ree::CompletionHandler.new(index, uri, node_context)
 
         dispatcher.register(self, :on_call_node_enter)
         dispatcher.register(self, :on_constant_read_node_enter)
@@ -26,27 +19,21 @@ module RubyLsp
         node_name = node.name.to_s
         return if node_name.size < CHARS_COUNT
 
-        completion_items = get_class_name_completion_items(node, @node_context, @index, @uri, CANDIDATES_LIMIT)
+        completion_items = @handler.get_class_name_completion_items(node)
         put_items_into_response(completion_items)
       end
 
       def on_call_node_enter(node)
         completion_items = []
-        ree_receiver = get_ree_receiver(node.receiver, @index)
+        ree_receiver = @handler.get_ree_receiver(node.receiver)
 
         if ree_receiver
-          completion_items = get_ree_object_methods_completions_items(ree_receiver, node.receiver, node)
+          completion_items = @handler.get_ree_object_methods_completions_items(ree_receiver, node.receiver, node)
         else
           return if node.receiver
           return if node.name.to_s.size < CHARS_COUNT
-
-          ree_objects = ReeObjectFinder.search_objects(@index, node.name.to_s, CANDIDATES_LIMIT)
-
-          return if ree_objects.size == 0
   
-          parsed_doc = RubyLsp::Ree::ParsedDocumentBuilder.build_from_ast(@node_context.parent, @uri)
-  
-          completion_items = get_ree_objects_completions_items(ree_objects, parsed_doc, node)
+          completion_items = @handler.get_ree_objects_completions_items(node)
         end
         
         put_items_into_response(completion_items)
