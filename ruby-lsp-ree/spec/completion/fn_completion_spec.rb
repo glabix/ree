18d68178005a495c303dd233dfd6772a13f08629
Oleg Potapov <oleg0potapov+1@gmail.com>
2025-02-20 09:53:59 +0300

    basic completion spec

--- ruby-lsp-ree/spec/completion/fn_completion_spec.rb
+++ ruby-lsp-ree/spec/completion/fn_completion_spec.rb
@@ -1,7 +1,38 @@
 # frozen_string_literal: true
 
-# RSpec.describe RubyLsp::Ree::CompletionListener do
-#   it "has a version number" do
-#     expect(ReeLib::VERSION).not_to be nil
-#   end
-# end
+require 'spec_helper'
+
+RSpec.describe "RubyLsp::Ree::CompletionListener" do
+  it "returns correct result for fn autocomplete request" do
+    source =  <<~RUBY
+      class SomeClass
+        def something
+          second
+        end
+      end
+    RUBY
+
+    with_server(source) do |server, uri|
+      server.process_message(
+        id: 1,
+        method: "textDocument/completion",
+        params: {
+          textDocument: {
+            uri: uri.to_s,
+          },
+          position: {
+            line: 3,
+            character: 5
+          }
+        }
+      )
+
+      result = server.pop_response
+      pp result
+      result = server.pop_response
+      pp result
+      result = server.pop_response
+      pp result
+    end
+  end
+end
--- ruby-lsp-ree/spec/spec_helper.rb
+++ ruby-lsp-ree/spec/spec_helper.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
-require "ruby_lsp_ree"
 require "sorbet-runtime"
 require "ruby_lsp/test_helper"
+require "ruby_lsp/internal"
+require "ruby_lsp_ree"
+require "ruby_lsp/ruby_lsp_ree/addon"
 
 RSpec.configure do |config|
+  include RubyLsp::TestHelper
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
