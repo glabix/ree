d9e202e8b7cb0f45fd173282ba14d12a1c6e1ac8
Oleg Potapov <oleg0potapov+1@gmail.com>
2025-02-28 14:42:47 +0300

    support rspec template

--- ruby-lsp-ree/lib/ruby_lsp/ruby_lsp_ree/ree_template_applicator.rb
+++ ruby-lsp-ree/lib/ruby_lsp/ruby_lsp_ree/ree_template_applicator.rb
@@ -5,6 +5,7 @@ module RubyLsp
 
       TEMPLATES_FOLDER = '.vscode-ree/templates'
       DEFAULT_TEMPLATE_FILENAME = 'default.rb'
+      RSPEC_TEMPLATE_PATH = 'spec_template.rb'
 
       def initialize
         return unless template_dir_exists?
@@ -24,18 +25,20 @@ module RubyLsp
         uri = change_item[:uri]
         path = URI.parse(uri).path
 
-        return if path.end_with?('_spec.rb') # skip spec templates for now
-
         file_content = File.read(path)
         return if file_content.size > 0
 
-        template_type = get_template_type_from_uri(uri)
-        return unless template_type
-
-        template_str = fetch_template(template_type)
-
-        template_info = fetch_template_info(uri)
-
+        if path.end_with?('_spec.rb')
+          template_str = fetch_rspec_template
+          template_info = fetch_rspec_template_info(uri)
+        else
+          template_type = get_template_type_from_uri(uri)
+          return unless template_type
+  
+          template_str = fetch_template(template_type)
+          template_info = fetch_template_info(uri)
+        end
+        
         template_content = replace_placeholders(template_str, template_info)
 
         File.write(path, template_content)
@@ -49,6 +52,10 @@ module RubyLsp
 
       def fetch_template(template_type)
         File.read(File.join(TEMPLATES_FOLDER, template_type, DEFAULT_TEMPLATE_FILENAME))
+      end
+
+      def fetch_rspec_template
+        File.read(File.join(TEMPLATES_FOLDER, RSPEC_TEMPLATE_PATH))
       end
 
       def fetch_template_info(uri)
@@ -65,6 +72,22 @@ module RubyLsp
         }
       end
 
+      def fetch_rspec_template_info(uri)
+        object_name = File.basename(uri, '.rb')
+        object_class = object_name.split('_').collect(&:capitalize).join
+        package_name = package_name_from_spec_uri(uri)
+        package_class = package_name.split('_').collect(&:capitalize).join
+        file_path = spec_relative_file_path_from_uri(URI(uri))&.delete_suffix('_spec')
+        
+        {
+          'RELATIVE_FILE_PATH' => file_path,
+          'MODULE_NAME' => package_class,
+          'CLASS_NAME' => object_class,
+          'OBJECT_NAME' => object_name, 
+          'PACKAGE_NAME' => package_name,
+        }
+      end
+      
       def replace_placeholders(template_str, template_info)
         template_info.each do |k,v|
           template_str.gsub!(k, v)
