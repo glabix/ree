c643acf267c5e64e4e96f4a8b987b95052058395
Oleg Potapov <oleg0potapov+1@gmail.com>
2025-03-06 13:03:17 +0300

    go-to-definition correct package

--- ruby-lsp-ree/spec/definition/fn_definition_spec.rb
+++ ruby-lsp-ree/spec/definition/fn_definition_spec.rb
@@ -24,4 +24,31 @@ RSpec.describe "RubyLsp::Ree::DefinitionListener" do
       expect(result.response.first.uri).to eq('file:///seconds_ago.rb')
     end
   end
+
+  it "returns correct package uri for similar fns in different packages" do
+    source =  <<~RUBY
+      class SomeClass
+        fn :some_class do
+          link :seconds_ago, from: :package2
+        end
+
+        def something
+          seconds_ago
+        end
+      end
+    RUBY
+
+    with_server(source) do |server, uri|
+      index_fn(server, 'seconds_ago', 'package1')
+      index_fn(server, 'seconds_ago', 'package2')
+      index_fn(server, 'seconds_ago', 'package3')
+
+      send_definition_request(server, uri, { line: 6, character: 5 })
+      
+      result = server.pop_response
+
+      expect(result.response.size).to eq(1)
+      expect(result.response.first.uri).to eq('file:///package2/package/package2/seconds_ago.rb')
+    end
+  end
 end
