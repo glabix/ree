a795a4287e12c271f8a506114b8d9088b2b352e2
Oleg Potapov <oleg0potapov+1@gmail.com>
2025-03-24 13:37:48 +0300

    fix rename handler

--- ruby-lsp-ree/lib/ruby_lsp/ruby_lsp_ree/ree_rename_handler.rb
+++ ruby-lsp-ree/lib/ruby_lsp/ruby_lsp_ree/ree_rename_handler.rb
@@ -8,8 +8,11 @@ module RubyLsp
       end
 
       def call(changes)
-        old_path = get_uri_path(changes.detect{ _1[:type] == Constant::FileChangeType::DELETED }[:uri])
-        new_path = get_uri_path(changes.detect{ _1[:type] == Constant::FileChangeType::CREATED }[:uri])
+        old_uri = changes.detect{ _1[:type] == Constant::FileChangeType::DELETED }[:uri]
+        new_uri = changes.detect{ _1[:type] == Constant::FileChangeType::CREATED }[:uri]
+      
+        old_path = get_uri_path(old_uri)
+        new_path = get_uri_path(new_uri)
 
         old_file_name = File.basename(old_path, '.rb').gsub(" copy.rb", ".rb")
         new_file_name = File.basename(new_path, '.rb')
@@ -17,6 +20,7 @@ module RubyLsp
         return if old_file_name == new_file_name
 
         parsed_doc = RubyLsp::Ree::ParsedDocumentBuilder.build_from_uri(new_uri)
+
         return if !parsed_doc || !parsed_doc.class_node
 
         old_class_name = old_file_name.split('_').collect(&:capitalize).join
@@ -24,7 +28,7 @@ module RubyLsp
 
         return unless parsed_doc.class_name == old_class_name
 
-        file_content_lines = File.read(new_uri.path).lines
+        file_content_lines = File.read(new_path).lines
         
         class_line = parsed_doc.class_node.location.start_line - 1
 
@@ -36,7 +40,7 @@ module RubyLsp
           file_content_lines[links_container_node_line].gsub!(/\b#{old_file_name}\b/, new_file_name)
         end
 
-        File.write(new_uri.path, file_content_lines.join)
+        File.write(new_path, file_content_lines.join)
       end
     end
   end
