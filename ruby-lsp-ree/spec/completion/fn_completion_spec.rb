f3804b7839f3f43b81a8452853a060c2f228a2e2
Oleg Potapov <oleg0potapov+1@gmail.com>
2025-03-28 17:28:51 +0300

    fix class includes parsing

--- ruby-lsp-ree/spec/completion/fn_completion_spec.rb
+++ ruby-lsp-ree/spec/completion/fn_completion_spec.rb
@@ -22,4 +22,28 @@ RSpec.describe "RubyLsp::Ree::CompletionListener" do
       expect(result.response.first.label).to eq('seconds_ago')
     end
   end
+
+  it "returns correct result for class includeing link dsl" do
+    source =  <<~RUBY
+      class SomeClass
+        include Ree::LinkDSL
+        include Enumerable
+
+        def something
+          second
+        end
+      end
+    RUBY
+
+    with_server(source) do |server, uri|
+      index_fn(server, 'seconds_ago')
+
+      send_completion_request(server, uri, { line: 5, character: 5 })
+      
+      result = server.pop_response
+      expect(result.response.size).to eq(1)
+      expect(result.response.first.label).to eq('seconds_ago')
+      expect(result.response.first.additional_text_edits.first.new_text).to match('link :seconds_ago')
+    end
+  end
 end
