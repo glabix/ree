88ff20ca4b745beb6454ea4eed8108b7be40c233
Oleg Potapov <oleg0potapov+1@gmail.com>
2025-05-15 18:06:47 +0300

    add spec

---
+++ ruby-lsp-ree/spec/definition/const_definition_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+RSpec.describe "RubyLsp::Ree::DefinitionListener" do
+  let(:entity_file_name){ sample_package_entities_dir + '/user.rb' }
+  
+  it "returns correct result for const definition request" do
+    source =  <<~RUBY
+      class SomeClass
+        fn :some_class do
+          link :some_package_fn, from: :some_package, import: -> { User }
+        end
+
+        def something
+          User
+        end
+      end
+    RUBY
+
+    file_uri = URI("file:///some_package/package/some_package/some_package_fn.rb")
+
+    with_server(source) do |server, uri|
+      index_fn(server, 'some_package_fn', 'some_package')
+      index_class(server, 'User', file_uri)
+
+      send_definition_request(server, uri, { line: 6, character: 5 })
+      
+      result = server.pop_response
+
+      expect(result.response.size).to eq(1)
+      expect(result.response.first.uri).to eq(file_uri.to_s)
+    end
+  end
+end
